af48db113be007cc4d51f567de68ce9ccb4ee4bf
Vaibhav Singh <vaibhavsingh2633@gmail.com>
2024-12-10 11:27:08 +0530

    Update ShayariGrid.jsx

--- src/components/ShayariGrid.jsx
+++ src/components/ShayariGrid.jsx
@@ -6,7 +6,7 @@ export function ShayariGrid({ shayaris }) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4">
       {shayaris.map((shayari) => (
         <ShayariCard key={shayari._id} shayari={shayari} />
       ))}
